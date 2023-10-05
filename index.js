2edede0ddceaa9126faaf2d1ffd4747bd1133a7b
chupacambra <jovankoul@hotmail.com>
2023-10-05 18:03:16 +0300

    Finished Server

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-const logEvents = require('./logEvents.js');
-const eventEmitter = require('node:events');
+/* const fsPromises = require('node:fs').promises;
+const path = require('node:path');
+const filePath = path.join(__dirname, 'files', 'starter.txt');
 
-class MyEmitter extends eventEmitter {}
-const myEmitter = new MyEmitter();
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(filePath, 'utf8');
+    console.log(data);
+    await fsPromises.unlink(filePath, 'utf8');
+    await fsPromises.writeFile(
+      path.join(__dirname, 'files', 'promiseWrite.txt'),
+      'henlo from promiseWrite \n'
+    );
+    await fsPromises.appendFile(
+      path.join(__dirname, 'files', 'promiseWrite.txt'),
+      'henlo from promiseAppend \n'
+    );
+    await fsPromises.rename(
+      path.join(__dirname, 'files', 'promiseWrite.txt'),
+      path.join(__dirname, 'files', 'promiseRename.txt')
+    );
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, 'files', 'promiseRename.txt'),
+      'utf8'
+    );
+    console.log(newData);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-myEmitter.on('log', (msg) => {
-  logEvents(msg);
-});
+fileOps();
 
-setTimeout(() => {
-  myEmitter.emit('log', 'Event Emitted');
-}, 2000);
+
+ */
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+if (!fs.existsSync('./new')) {
+  fs.mkdirSync('./new', (err) => {
+    if (err) {
+      throw err;
+    } else console.log('directory created');
+  });
+  fs.writeFile(path.join(__dirname, 'new', 'test.txt'), 'henlo fren', (err) => {
+    if (err) {
+      throw err;
+    } else console.log('directory created');
+  });
+}
+
+/* fs.writeFile(path.join(__dirname, 'new', 'test.txt'), 'hi', (err) => {
+  if (err) {
+    console.error(err);
+  }
+}); */
--- logEvents.js
+++ logEvents.js
@@ -1,25 +1,22 @@
-//npm
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
-//local modules
-const fs = require('node:fs');
-const fsPromises = require('node:fs').promises;
+
+const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
-const logEvents = async (message) => {
-  const dateTime = `${format(new Date(), 'yyyyMMdd \t HH:mm:ss')}`;
-  const logItem = `${dateTime} \t ${uuid()} \t ${message} \n`;
+const logEvents = async (message, logName) => {
+  const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
   try {
     if (!fs.existsSync(path.join(__dirname, 'logs'))) {
       await fsPromises.mkdir(path.join(__dirname, 'logs'));
-      await fsPromises.writeFile('event.text', logItem);
     }
-    await fsPromises.appendFile(
-      path.join(__dirname, 'logs', 'event.txt'),
-      logItem
-    );
-  } catch (error) {
-    console.log(error);
+
+    await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+  } catch (err) {
+    console.log(err);
   }
 };
 
